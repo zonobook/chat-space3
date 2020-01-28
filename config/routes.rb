a6c363ee4993817827618b99343a9fe7128c52d7
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2020-01-28 13:29:30 +0900

    jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
+  namespace :api do
+    resources :messages, only: :index, defaults: { format: 'json' }
+  end
 end
