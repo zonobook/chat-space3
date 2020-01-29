ae626dcf50c6b0f2505fdae5417c4559c8b6c48a
tsuyoshi-uezono <tsuyoshi.uezono@di-v.co.jp>
2020-01-29 20:32:53 +0900

    end

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-  end
-  namespace :api do
-    resources :messages, only: :index, defaults: { format: 'json' }
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
